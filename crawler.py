27ccbb09515ab89b5e8f213d2c9381aba2d0d40f
OAOv <leo877.OAO@gmail.com>
2018-08-24 10:10:32 +0800

    add dcard fitness(hot) crawler

--- crawler.py
+++ crawler.py
@@ -25,9 +25,24 @@ def webhook_handler():
         chat_id = update.message.chat_id
         text = update.message.text
 
-        bot.sendMessage(chat_id = chat_id, text = text)
+        if text == '/dFitness':
+            bot.sendMessage(chat_id = chat_id, text = dcardFitness())
+        else:
+            bot.sendMessage(chat_id = chat_id, text = text)
 
     return 'ok'
+
+def dcardFitness():
+	url = "https://www.dcard.tw/f/fitness"
+    retext = "dcard fitness\n"
+    resp = requests.get(url)
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    fitnessTitle = soup.find_all('h3', re.compile('PostEntry_title_'))
+    fitnessLink = soup.find_all('a', re.compile('PostEntry_root_'))
+
+    for index, (title, link) in enumerate(zip(fitnessTitle[:10], fitnessLink[:10])):  #前10篇
+        retext += "{0:2d}. {1}\nhttps://www.dcard.tw{2}\n".format(index + 1, title.text, link.get('href'))
+    return retext
 
 if __name__ == "__main__":
     _set_webhook()
