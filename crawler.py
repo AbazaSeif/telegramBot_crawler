f4d3853e587dcbb7c0463808519fdc9e16b6c3d4
OAOv <leo877.OAO@gmail.com>
2018-08-24 09:23:34 +0800

    test bot

--- crawler.py
+++ crawler.py
@@ -10,3 +10,26 @@ app = Flask(__name__)
 #bot Token
 botToken = '681432558:AAEdVCNPNr4AudRvqwhk1Sqmk2hACHpkgWI'
 bot = telegram.Bot(token = botToken)
+
+def _set_webhook():
+    status = bot.set_webhook('URL')
+	if not status:
+        print('Webhook setup failed')
+        sys.exit(1) 
+	pass
+
+@app.route('/hook', methods=['POST'])
+def webhook_handler():
+    if request.method == 'POST':
+        update = telegram.Update.de_json(request.get_json(force=True), bot)
+
+        chat_id = update.message.chat_id
+        text = update.message.text
+
+        bot.sendMessage(chat_id = chat_id, text = text)
+
+    return 'ok'
+    
+if __name__ == "__main__":
+    _set_webhook()
+    app.run()
